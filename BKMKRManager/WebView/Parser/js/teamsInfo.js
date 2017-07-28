cf57650255ab70b05ed6f79f4f6523e16182014d
chemer <chemer@ua.fm>
2017-07-29 01:01:40 +0300

    repair teams info web parsing

--- BKMKRManager/WebView/Parser/js/teamsInfo.js
+++ BKMKRManager/WebView/Parser/js/teamsInfo.js
@@ -1,20 +1,9 @@
 function teamsInfo() {
     var result = '<[Team1Name]>&&<[Team2Name]>';
 
-    var head = document.getElementsByClassName('mrk-head');
-    var item = head[0].getElementsByClassName('mrk-itm');
-    var m1 = item[0].getElementsByClassName('m_1');
-    if(m1.length > 0) {
-        var m3 = item[0].getElementsByClassName('m_3');
-        
-        result = result.replace('<[Team1Name]>', m1[0].innerText.split("\n")[0]);
-        result = result.replace('<[Team2Name]>', m3[0].innerText.split("\n")[0]);
-    } else {
-        var ttt = item[0].getElementsByClassName('ttt');
-
-        result = result.replace('<[Team1Name]>', ttt[0].innerText.split(" - ")[0]);
-        result = result.replace('<[Team2Name]>', ttt[0].innerText.split(" - ")[1]);
-    }
+    var teamsInfo = $('.event--name--info')[0].innerText.split("\n");
+    result = result.replace('<[Team1Name]>', teamsInfo[0]);
+    result = result.replace('<[Team2Name]>', teamsInfo[1]);
     
     return result;
 }
